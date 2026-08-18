373647ecfd40e77a2af1caae7973e633bf14bd93
djafarxyxy <farhanch12344@gmail.com>
2026-08-18 15:23:15 +0700

    tambah case 4 keluar sama default

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -127,6 +127,13 @@ int main() {
                     q.display();
                     break;
                 }
+                case '4': {
+                    return 0;
+                }
+                default: {
+                    cout << "Invalid option!!!" << endl;
+                    break;
+                }
             }
         }
     }
