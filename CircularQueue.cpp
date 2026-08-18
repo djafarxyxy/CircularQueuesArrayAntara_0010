d831291708f208e65e088636bb5acef0be618324
djafarxyxy <farhanch12344@gmail.com>
2026-08-18 15:23:16 +0700

    tambah try catch biar aman dari input aneh

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -136,6 +136,9 @@ int main() {
                 }
             }
         }
+        catch (exception& e) {
+            cout << "Check for the values entered." << endl;
+        }
     }
     return 0;
 }
