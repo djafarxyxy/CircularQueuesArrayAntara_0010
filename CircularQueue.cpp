5b5a7cd0f6159c97b0d0b6600b990a00111d4295
djafarxyxy <farhanch12344@gmail.com>
2026-08-18 15:21:22 +0700

    tambah case 1 buat insert

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -109,6 +109,16 @@ int main() {
             cout << "Enter your choice (1-4): ";
             cin >> ch;
             cout << endl;
+            switch (ch) {
+                case '1': {
+                    q.insert();
+                    break;
+                }
+                case '2': {
+                    q.remove();
+                    break;
+                }
+            }
         }
     }
     return 0;
