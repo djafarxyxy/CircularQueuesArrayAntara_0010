87ab9137f4d4fb2e0b354558c7bc5322326b3400
djafarxyxy <farhanch12344@gmail.com>
2026-08-18 15:21:16 +0700

    tambah method display buat cek kosong

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -58,6 +58,17 @@ public:
         }
     }
 
+    void display() {
+        int FRONT_Position = FRONT;
+        int REAR_Position = REAR;
+
+        // cek apakah antrian kosong
+        if (FRONT_Position == -1) {
+            cout << "Queue is empty\n";
+            return;
+        }
+}
+
 int main() {
     return 0;
 }
